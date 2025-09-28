d92e5955f4ea7cdf041e4c24db664e9cbbbd8110
Pavel <pavlik.shevchenko@mail.ru>
2025-09-28 18:16:56 +0300

    Добавление файлов проекта

---
+++ Files_archiver/Files_archiver.cpp
@@ -0,0 +1,12 @@
+﻿// Files_archiver.cpp: определяет точку входа для приложения.
+//
+
+#include "Files_archiver.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
